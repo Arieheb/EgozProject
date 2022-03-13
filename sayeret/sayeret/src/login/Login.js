a0f171980ff2f38d6652270050def83da72ec97c
Idan Kideckel <idanbkideckel@gmail.com>
2022-03-13 16:18:32 +0200

    login

---
+++ sayeret/sayeret/src/login/Login.js
@@ -0,0 +1,11 @@
+import "./Login.css"
+
+export function Login(){
+    return (
+        <div id="login">
+            <input type = "text"></input>
+            <input type = "password"></input>
+            <button></button>
+        </div>
+    );
+}
