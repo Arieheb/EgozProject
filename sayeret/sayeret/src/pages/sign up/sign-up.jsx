75d6f95983667dbd294ccdc62e71b8790ee36ae4
Noam Haviv Hamawi <noamhama@gmail.com>
2022-03-20 15:44:59 +0200

    changes

--- sayeret/sayeret/src/pages/sign up/sign-up.jsx
+++ sayeret/sayeret/src/pages/sign up/sign-up.jsx
@@ -1,11 +1,14 @@
 import "./sign-up.css"
-export function Login(){
+import React, {Component} from 'react';
+import MonthYearPocker from 'react-month-year-picker';
+export function signUp(){
     return(
     <div id="sign-up">
         <input type = "text" plaeceholder="userNames">שם פרטי ומשפחה</input>
         <input type = "password" plaeceholder="passowrd">סיסמא</input>
         <input type="password" plaeceholder="passowrd">חזור על הסיסמא</input>
         <button id= "continue-button">המשך</button>
+        <input type="text" />
     </div>
     );
 }
