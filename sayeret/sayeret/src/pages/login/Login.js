4fda5a496fc7f6d874cffc15198c2cb95a2700e4
Avraham Kaplan <99396617+AviLinko@users.noreply.github.com>
2022-04-13 12:18:55 +0300

    .

--- sayeret/sayeret/src/pages/login/Login.js
+++ sayeret/sayeret/src/pages/login/Login.js
@@ -1,2 +1,24 @@
-import react from 'react';
-import {View,StyleSheet} from 'react-native';
+import react from 'react'
+import {View,StyleSheet,Image} from 'react-native'
+import Logo from '../images/logo.png'
+
+const LoginScreen = () => {
+    return (
+        <View style={styles.container}>
+            <View>
+                <Image source={Logo} styles={styles.logo} resizeMode="contain"/>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container:{
+        alignItems:'center',
+        padding:30,
+    },
+    logo:{
+        width:'70%',
+        height: 100,
+    },
+})
