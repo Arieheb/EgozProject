9e820c96d625ccfc343d6801c4f5c98499628cf0
Avraham Kaplan <99396617+AviLinko@users.noreply.github.com>
2022-04-26 15:31:43 +0300

    signIn

--- sayeret/pages/Login/Login.js
+++ sayeret/pages/Login/Login.js
@@ -1,5 +1,5 @@
 import {React,useState,} from 'react';
-import {View,StyleSheet,Image,TextInput,Button} from 'react-native';
+import {View,StyleSheet,Image,TextInput,Button,Text,TouchableOpacity} from 'react-native';
 import Logo from '../../assets/Images/logo.png';
 import {validate} from 'react-email-validator';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
@@ -20,28 +20,34 @@ const LoginScreen = () => {
     }
     return (
         <View style={styles.container}>
-            <View>
-                <Image source={Logo} 
+            <View style = {styles.top}>
+                <Image style = {styles.logo} source={Logo} 
                 styles={styles.logo} 
-                resizeMode="contain"
                 />
             </View>
-            <View>
-                <TextInput placeholder='אימייל:'
-                style={styles.input}
-                value={email}
-                onChangeText={text=>setEmail(text)}
-                />
-                <TextInput placeholder='סיסמא:' 
-                style={styles.input}
-                value={password}
-                onChangeText={text=>setPassword(text)}
-                secureTextEntry
-                />
-            </View>
-            <View>
-                <Button title='היכנס' style={styles.button}></Button>
-                <Button title='הרשמה' style={styles.button}></Button>
+            <View style = {styles.bottom}>
+                <Text style = {styles.heading}>כניסה</Text>
+                <View style = {styles.inputView}>
+                    <TextInput placeholder='Email:'
+                    style={styles.input}
+                    placeholderTextColor={"#fff"}
+                    value={email}
+                    onChangeText={text=>setEmail(text)}
+                    />
+                    <TextInput placeholder='Password:' 
+                    style={styles.input}
+                    placeholderTextColor={"#fff"}
+                    value={password}
+                    onChangeText={text=>setPassword(text)}
+                    secureTextEntry
+                    />
+                    <TouchableOpacity style = {styles.buttons}>
+                    <Text>היכנס</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style = {styles.buttons}>
+                    <Text>הירשם</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
     );
@@ -50,20 +56,60 @@ const LoginScreen = () => {
 const styles = StyleSheet.create({
     container:{
        flex:1,
+       marginTop:40,
+       flexDirection:'column',
        justifyContent:'center',
        alignItems:'center',
     },
-    logo:{
+    top:{
+       width:'100%',
+       height:'40%',
+       display:'flex',
+       alignItems:'center',
+    },
+    bottom:{
+        width:'100%',
+        height:'60%',
+        backgroundColor:'#31386C',  
+        borderTopLeftRadius:25,
+        borderTopRightRadius:25,
         
-        
+    },
+    heading:{
+        color:'#fff',
+        fontSize:32,
+        fontWeight:'bold',
+        marginLeft:40,
+        marginTop:40,
+    },
+    inputView:{
+        width:'100%',
+        display:'flex',
+        flexDirection:'column',
+        alignItems:'center',
+        marginTop:35,
     },
     input: {
-        
-        
+       width:'90%',
+       borderWidth:1,
+       borderColor:'#fff',
+       height:52,
+       borderRadius:8,
+       paddingRight:15,
+       margin:5,
     },
-    button:{
-        padding:20,
-        marginTop:10,
+    buttons:{
+       alignItems:'center',
+       width:'90%',
+       color:'blue',
+       height:40,
+       backgroundColor:'#fff',
+       marginTop:10,
+       borderRadius:8,
+       display:'flex',
+       alignItems:'center',
+       justifyContent:'center',
+
         
     }
     
