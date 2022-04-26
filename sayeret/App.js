85cf7806b671d30d8901f517720a50741850d844
Idan Kideckel <idanbkideckel@gmail.com>
2022-04-26 21:25:45 +0300

    update

--- sayeret/App.js
+++ sayeret/App.js
@@ -1,15 +1,27 @@
 import 'react-native-gesture-handler';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import LoginScreen from './pages/Login/Login' 
 import Forum from './pages/Forum/Forum';
-import SignUpScreen from './pages/Login/SignUp' 
+import Home from './pages/home/Home';
+import Jobs from './pages/jobs/Jobs';
+import About from './pages/About/About';
 
-// const Drawer = createDrawerNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-     <SignUpScreen/>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name='login' component={LoginScreen}/>
+        <Drawer.Screen name='Home' component={Home}/>
+        <Drawer.Screen name='jobs' component={Jobs}/>
+        <Drawer.Screen name='about' component={About}/>
+        <Drawer.Screen name='forum' component={Forum}/>
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
