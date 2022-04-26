b76078fad23590b4e2bb56bdec11ee2cd3e5cf46
Arieheb <Arieh.berlin@gmail.com>
2022-04-26 13:32:34 +0300

    events+myinfo

---
+++ sayeret/pages/myInfo/myInfo.js
@@ -0,0 +1,5 @@
+import React from "react";
+import {React,useState,} from 'react';
+import {View,StyleSheet,Image,TextInput,Button} from 'react-native';
+//import Logo from '../../assets/Images/logo.png';
+import {validate} from 'react-email-validator';
