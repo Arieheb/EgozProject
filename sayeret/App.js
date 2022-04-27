86c87ef14de9a2ed59637f8384eb6c85df4bf89f
Idan Kideckel <idanbkideckel@gmail.com>
2022-04-27 12:24:16 +0300

    .

--- sayeret/App.js
+++ sayeret/App.js
@@ -1,21 +1,46 @@
 import 'react-native-gesture-handler';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import LoginScreen from './pages/Login/Login' 
 import Forum from './pages/Forum/Forum';
 import Home from './pages/home/Home';
 import Jobs from './pages/jobs/Jobs';
 import About from './pages/About/About';
+import SignUpScreen from './pages/Login/SignUp'
 
 const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
+  const [user, setUser] = useState();
+
+  // // Handle user state changes
+  // function onAuthStateChanged(user) {
+  //   setUser(user);
+  // }
+  // useEffect(() => {
+  //   const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+  //   return subscriber; // unsubscribe on unmount
+  // }, []);
+
+  if(!user){
+    return(
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='login' component={LoginScreen}/>
+          <Stack.Screen name='SignUp' component={SignUpScreen}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
   return (
     <NavigationContainer>
       <Drawer.Navigator>
-        <Drawer.Screen name='login' component={LoginScreen}/>
         <Drawer.Screen name='Home' component={Home}/>
         <Drawer.Screen name='jobs' component={Jobs}/>
         <Drawer.Screen name='about' component={About}/>
