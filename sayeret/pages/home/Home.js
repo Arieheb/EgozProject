1178888b0dfbc31064d12b1c7d657147d3a9c38f
ChenMoskovitz <chen.mosko.cm@gmail.com>
2022-05-09 01:41:01 +0300

    Update Home.js

--- sayeret/pages/home/Home.js
+++ sayeret/pages/home/Home.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import { View,Text, TouchableOpacity, StyleSheet, ScrollView} from 'react-native';
 import YoutubePlayer from 'react-native-youtube-iframe';
+import Icon from "react-native-vector-icons/FontAwesome5"
+
+const NumberCard = props=>{
+    return(
+        <View style={{alignItems: "center"}}>
+            <Text style={styles.title}>{props.num}</Text>
+            <Text style={styles.content}>{props.title}</Text>
+        </View>
+    )
+}
+
+const GoalCard = props=>{
+    return(
+        <View style={{alignItems: "center"}}>
+            <View style = {styles.icon}>
+
+            </View>
+            <Text style={styles.title}>{props.title}</Text>
+            <Text style={styles.content}>{props.description}</Text>
+        </View>
+    )
+}
 
 const Home = props=>{
     return(
@@ -21,9 +43,32 @@ const Home = props=>{
                     <Text>קרא עוד</Text>
                 </TouchableOpacity>
             </View>
-            <View>
-                
+            <View style={styles.stat}>
+                <View>
+              <NumberCard num = "45" title = "שנות פעילות"/>
+              <NumberCard num = "2,172" title = "חברים בעמותה"/>
+                </View>
+                <View>
+                    <NumberCard num = "16" title = "פרויקטים פעילים"/>
+                    <NumberCard num = "9,342" title = "בוגרי יחידה"/>
+              </View>
+              
+
             </View>
+            <View>
+                <Text style = {styles.title}>המטרות שלנו</Text>
+                <GoalCard title = "הנצחת חיילי היחידה" description = "תחזוק אתר ההנצחה בקלעת נמרוד, קיום יום משפחות שנתי עם היחידה, ספר מורשת ותמיכת המשפחות השכולות."/>
+                <GoalCard title = "שימור הקשר בין בוגרי היחידה" description = "בעזרת מפגשי הבוגרים ורשת הנטוורקינג של בוגרי היחידה תסייע לך במציאת עבודה, שותפויות עסקיות ויזמות חברתית."/>
+                <GoalCard title = "דאגה למשתחררים ופצועים מהיחידה" description = "אנו מסייעים לפרויקטי המשתחררים ומקיימים סדנת “שחרור נעים” לשחרור חלק וסיוע בהשמה לעבודה."/>
+                <GoalCard title = "ליווי היחידה ולוחמיה הסדירים" description = "ליווי במהלך המסלול ומתן שי למסיימי מסלול. שימור המורשת – מור”קים ודאגה לחיילים עם בעיות כלכליות."/>
+            </View>
+            <View>
+                <Text style = {styles.title} >אירועים</Text>
+            </View>
+            <View>
+                <Text style = {styles.title}>יזכור</Text>
+            </View>
+
         </ScrollView>
     );
 }
@@ -40,6 +85,17 @@ const styles = StyleSheet.create({
     },
     content: {
         fontSize: 20
+    },
+    stat: {
+        flexDirection: "row",
+        backgroundColor: "darkgray",
+        justifyContent: "space-around"
+    },
+    icon: {
+        borderRadius: 50,
+        backgroundColor: "blue",
+        width: 100,
+        height: 100,
     }
 });
 
