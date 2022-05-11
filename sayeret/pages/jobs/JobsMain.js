167da571a4a9eabe323b62169c50fa3f1ab6cab9
avivhaish <avivsh93@gmail.com>
2022-05-11 22:02:02 +0300

    JobCard update
    Added contact info to the JobCard component:
    - Contact name
    - Contact phone
    - Contact email

--- sayeret/pages/jobs/JobsMain.js
+++ sayeret/pages/jobs/JobsMain.js
@@ -4,10 +4,10 @@ import JobCard from './JobCard';
 
 const JobsMain = props=>{
     const [jobsList, updateJobsList] = useState([
-        {id:0,title:"מתכנת/ת dot.net",location:"פתח תקווה",description:"דרוש/ה מתכנת/ת לחברה תעשייתית בתחום האלקטרוניקה בפתח תקווה, כפיפות ישירה למנהל IT, משרה מלאה ללא ימי שישי, תנאים מעולים למתאימים/ות."},
-        {id:1,title:"מתכנת/ת ++C",location:"נתניה",description:"לצוות פיתוח איכותי המפתח פתרונות עבור חברת אנרגיה קמעונאית באזור השרון דרוש /ה מתכנת /ת בעל /ת ניסיון."},
-        {id:2,title:"מתכנת/ת Fullstack",location:"ירושלים",description:"לנעה מערכות בעמ דרושים /ות מתכנתים /ות Fullstack ו- Backend. דרוש /ה CTO שייכנס / תכנס כ- Founder ודרושים מתכנתים /ות Fullstack ו- Backend."},
-        {id:3,title:"מנהל אורחן",location:"קרית מוצקין",description:"לאורחן משיח דרוש מנהל, זמר ופרזנטור ששיאו מאחוריו, ויעשה הכל כדי להיות בטופ. דרישות תפקיד: שחצן, וולגרי ובעל ביטחון עצמי מופרז."}
+        {id:0,title:"מתכנת/ת dot.net",location:"פתח תקווה", contactName: "לירון", contactPhone:"0509397547", contactEmail:"lirona@eltek.com", description:"דרוש/ה מתכנת/ת לחברה תעשייתית בתחום האלקטרוניקה בפתח תקווה, כפיפות ישירה למנהל IT, משרה מלאה ללא ימי שישי, תנאים מעולים למתאימים/ות."},
+        {id:1,title:"מתכנת/ת ++C",location:"נתניה",contactName: "תומר", contactPhone:"0545782209", contactEmail:"tomerco@amdocs.com", description:"לצוות פיתוח איכותי המפתח פתרונות עבור חברת אנרגיה קמעונאית באזור השרון דרוש /ה מתכנת /ת בעל /ת ניסיון."},
+        {id:2,title:"מתכנת/ת Fullstack",location:"ירושלים",contactName: "אפרת", contactPhone:"0505488023", contactEmail:"efrata@lightricks.com", description:"לנעה מערכות בעמ דרושים /ות מתכנתים /ות Fullstack ו- Backend. דרוש /ה CTO שייכנס / תכנס כ- Founder ודרושים מתכנתים /ות Fullstack ו- Backend."},
+        {id:3,title:"מנהל אורחן",location:"קרית מוצקין",contactName: "משיח", contactPhone:"0509321083", contactEmail:"lamalo@orhanmashiah.com", description:"לאורחן משיח דרוש מנהל, זמר ופרזנטור ששיאו מאחוריו, ויעשה הכל כדי להיות בטופ. דרישות תפקיד: שחצן, וולגרי ובעל ביטחון עצמי מופרז."}
     ]);
 
     function goToAddJob (){
@@ -21,7 +21,7 @@ const JobsMain = props=>{
                     contentContainerStyle={{alignItems:"center"}}
                     data={jobsList}
                     renderItem={({item}) => {
-                        return <JobCard title={item.title} location={item.location} description={item.description}/>
+                        return <JobCard title={item.title} location={item.location} contactName={item.contactName} contactPhone={item.contactPhone} contactEmail={item.contactEmail} description={item.description}/>
                     }}
                 />  
                 <View style={{minHeight:80}}>
