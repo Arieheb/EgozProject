da61c7cab502f91dcb5cd331049557ae56828712
Idan Kideckel <idanbkideckel@gmail.com>
2022-05-12 11:25:02 +0300

    Update ForumMain.js

--- sayeret/pages/Forum/ForumMain.js
+++ sayeret/pages/Forum/ForumMain.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Text,Button, View, StyleSheet, FlatList,Modal} from 'react-native';
+import {Text,Button, View, StyleSheet, FlatList,Modal,SafeAreaView} from 'react-native';
 import { TouchableRipple,Avatar, shadow } from 'react-native-paper';
 import Profile from "../../assets/Images/profile.png"
 import WriteToForum from './forumWrite';
@@ -35,7 +35,7 @@ const ForumItem = props=>{
             
             {/* the chat room */}
             <Modal visible={visible} style={{height:"80",opacity:1}}>
-                <View style={styles.header}>   
+                <SafeAreaView style={styles.header}>   
                     <TouchableRipple onPress={()=>{setVisible(false)}}>
                         <Icon
                             name='keyboard-backspace'
@@ -48,7 +48,7 @@ const ForumItem = props=>{
                         style={{marginLeft:20,marginRight:10}}
                     />
                     <Text style={styles.name}>{props.name}</Text>
-                </View>
+                </SafeAreaView>
                 <WriteToForum/>
             </Modal>
         </View>    
@@ -104,7 +104,11 @@ const styles = StyleSheet.create({
         alignItems:"center",
         paddingVertical:15,
         backgroundColor:"#00aadd",
-        elevation:5
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 5 },
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        elevation: 5,
     }
 
 })
