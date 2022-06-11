7a65d57b5f09a5ed5977d2dde0a0178ad34374ab
avivhaish <avivsh93@gmail.com>
2022-06-11 22:58:19 +0300

    Update App.js
    Header color

--- sayeret/App.js
+++ sayeret/App.js
@@ -85,7 +85,7 @@ else{
     <NavigationContainer>
         <Drawer.Navigator drawerContent={props=><DrawerContent {...props}/>} 
         screenOptions={{drawerPosition:'right',/*title: null,*/headerStyle: {
-            backgroundColor: '#535F6D',
+            backgroundColor: '#485260',
           }, headerRight: () => <PayMember/>}}>
           <Drawer.Screen name='home' component={Home} options = {{title: null}}/>
           <Drawer.Screen name='jobs' component={Jobs} options = {{title: null}}/>
