5057084dc11a66fbb8427ab0ea3216da3fb75776
Idan Kideckel <idanbkideckel@gmail.com>
2022-06-11 20:16:47 +0300

    Update Contact.js

--- sayeret/pages/contact/Contact.js
+++ sayeret/pages/contact/Contact.js
@@ -7,6 +7,9 @@ const Contact = (props) => {
     return (
         <WebView 
             source={{uri:"https://shop117095.istores.co.il/contact/"}}
+            injectedJavaScript='document.getElementsByTagName("footer")[0].setAttribute("hidden", true);
+            document.getElementsByTagName("header")[0].setAttribute("hidden", true);
+            document.getElementsByClassName("form-group af-inner")[document.getElementsByClassName("form-group af-inner").length-2].setAttribute("hidden", true)'
         />
       )
 }
