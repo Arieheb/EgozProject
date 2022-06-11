f8dccf316fddc4047949474f63a0757c9d80e8a4
Idan Kideckel <idanbkideckel@gmail.com>
2022-06-11 12:10:28 +0300

    Update SignUp.js

--- sayeret/pages/Login/SignUp.js
+++ sayeret/pages/Login/SignUp.js
@@ -1,10 +1,9 @@
 import { View, Text ,TextInput, StyleSheet,TouchableOpacity,StatusBar,Image,Dimensions,Keyboard, KeyboardAvoidingView,Alert, Platform} from 'react-native'
-import {React,useState,useRef,useEffect} from 'react'
+import {React,useState,useRef} from 'react'
 import AppIntroSlider from 'react-native-app-intro-slider';
 import { FlatList, ScrollView } from 'react-native-gesture-handler';
 import { auth, db } from '../../firebase';
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import SignUpAuth from './SignUpAuth';
 import { addDoc, collection } from 'firebase/firestore'; 
 // import Logo from '../../assets/Images/signUpLogo.png';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
@@ -54,19 +53,37 @@ const SignUp = props => {
   const [generation ,setGeneration] = useState("")
   const [team ,setTeam] = useState("")
   const [why ,setWhy] = useState("")
-
-
+  const refference = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+  const lastNameRef = useRef();
+  const addressRef = useRef();
+  const cityRef = useRef();
+  const phoneRef = useRef();
+  const generationRef = useRef();
+  const teamRef =useRef();
   
-  // const validate = () => {
-  //     if(email=="" || password=="" ||confirmPassword == ""){
-  //       alert("אחד מהנתונים חסרים")
-  //       return
-  //     }
-  //     if(password!=confirmPassword){
-  //       alert("הסיסמאות אינן זהות")
-  //       return
-  //     }
-  // }
+  const validate = (page) => {
+    if(page == 1){
+      if(email=="" || password=="" ||confirmPassword == ""){
+        alert("אחד מהנתונים חסרים")
+        return false;
+      }
+      if(password!=confirmPassword){
+        alert("הסיסמאות אינן זהות")
+        return false;
+      }
+    }
+    if(page ==2){
+      if(firstName == "" || LastName == "" || address=="" || city == "" || phone == ""){
+        alert("אחד מהנתונים חסרים")
+        return false;
+      }
+        
+    }
+
+      return true;
+  }
   const renderItem = ({item}) => {
     const handleSignUp = () =>
       { 
@@ -147,8 +164,8 @@ const SignUp = props => {
     //----------------------------------------first page ----------------------------
     if(item.key == 1)
     return (
-    
-      <KeyboardAvoidingView style={styles.container} behavior="padding"> 
+      <ScrollView contentContainerStyle={{flexGrow: 1}}>
+      <View style={styles.container}> 
         <TouchableOpacity onPress={Keyboard.dismiss} activeOpacity={1}> 
           <View style = {styles.top}>
             <Image style = {styles.logo} source={Logo} />
@@ -160,14 +177,21 @@ const SignUp = props => {
               <TextInput placeholder='אימייל:'
                 style={styles.input}
                 placeholderTextColor={"#fff"}
+                returnKeyType ='next'
                 value={email}
                 onChangeText={text=>setEmail(text)}
+                onSubmitEditing={() => { passwordRef.current.focus(); }}
+                blurOnSubmit={false}
               />
               <TextInput placeholder='סיסמא:' 
                 style={styles.input}
                 placeholderTextColor={"#fff"}
+                returnKeyType ='next'
                 value={password}
                 onChangeText={text=>setPassword(text)}
+                ref = {input=>passwordRef.current = input}
+                onSubmitEditing={() => { confirmPasswordRef.current.focus(); }}
+                blurOnSubmit={false}
                 secureTextEntry
               />
               <TextInput placeholder='אימות סיסמא:' 
@@ -175,25 +199,30 @@ const SignUp = props => {
                 placeholderTextColor={"#fff"}
                 value={confirmPassword}
                 onChangeText={text=>setConfirmPassword(text)}
+                ref = {input=>confirmPasswordRef.current=input}
                 secureTextEntry
               />
               <TouchableOpacity 
               style = {styles.buttons} 
-              // onPress={() => {goToSlide(2,true)}}
-                  
+              onPress={() => {
+                if(validate(1))
+                  refference.current.goToSlide(1,true)
+              }}
               >
                 <Text style = {styles.buttonText} >המשך</Text>
               </TouchableOpacity>
             </View>
           </KeyboardAwareScrollView>
         </TouchableOpacity>
-      </KeyboardAvoidingView>  
+      </View>  
+      </ScrollView>
     
     );
       //-------------------------------- second page ----------------------------------------------------------------
       else if(item.key == 2)
       return ( 
-          <KeyboardAvoidingView style={styles.container} behavior="padding"> 
+        <ScrollView contentContainerStyle={{flexGrow: 1}}>
+          <View style={styles.container}> 
             <TouchableOpacity onPress={Keyboard.dismiss} activeOpacity={1}>
               <View style = {styles.top}>
               <Image style = {styles.logo} source={Logo} />
@@ -205,26 +234,41 @@ const SignUp = props => {
                  <TextInput placeholder='שם פרטי:'
                   style={styles.input}
                   placeholderTextColor={"#fff"}
+                  returnKeyType ='next'
                   value={firstName}
                   onChangeText={text=>setFirstName(text)}
+                  onSubmitEditing={() => { lastNameRef.current.focus(); }}
+                  blurOnSubmit={false}
                   />
                   <TextInput placeholder='שם משפחה:' 
                   style={styles.input}
                   placeholderTextColor={"#fff"}
+                  returnKeyType ='next'
                   value={LastName}
                   onChangeText={text=>setLastName(text)}
+                  ref = {input=>lastNameRef.current = input}
+                  onSubmitEditing={() => { addressRef.current.focus(); }}
+                  blurOnSubmit={false}
                   />
                 <TextInput placeholder='כתובת:' 
                   style={styles.input}
                   placeholderTextColor={"#fff"}
+                  returnKeyType ='next'
                   value={address}
                   onChangeText={text=>setAddress(text)}
+                  ref = {input=>addressRef.current = input}
+                  onSubmitEditing={() => { cityRef.current.focus(); }}
+                  blurOnSubmit={false}
                 />
                   <TextInput placeholder='עיר:' 
                   style={styles.input}
                   placeholderTextColor={"#fff"}
+                  returnKeyType ='next'
                   value={city}
                   onChangeText={text=>setCity(text)}
+                  ref = {input=>cityRef.current = input}
+                  onSubmitEditing={() => { phoneRef.current.focus(); }}
+                  blurOnSubmit={false}
                 />
                 <TextInput placeholder='מספר טלפון:' 
                   style={styles.input}
@@ -232,88 +276,102 @@ const SignUp = props => {
                   placeholderTextColor={"#fff"}
                   value={phone}
                   onChangeText={text=>setPhone(text)}
+                  ref = {input=>phoneRef.current = input}
                 />
                 
-                <TouchableOpacity style = {styles.buttons} 
-                 >
+                <TouchableOpacity style = {styles.buttons}
+                 onPress={()=>{
+                   if(validate(2))
+                    refference.current.goToSlide(2,true)
+                   }}>
                   <Text style = {styles.buttonText} >המשך</Text>
                 </TouchableOpacity>
             </View>
             </KeyboardAwareScrollView>
             </TouchableOpacity>
-          </KeyboardAvoidingView>
+          </View>
+          </ScrollView>
         
         );
         //----------------------------------------------third page ----------------------------------
       else
       return (
-          <KeyboardAvoidingView style={styles.container} behavior="padding"> 
-            <TouchableOpacity onPress={Keyboard.dismiss} activeOpacity={1}>
-              <View style = {styles.top}>
-              <Image style = {styles.logo} source={Logo} />
-                <Text style= {styles.heading}> שאלון אימות</Text>
-                
-                {/* <Text>נתונים אלו יישלחו למנהל המערכת לצורך אימות נתוניך</Text> */}
-              </View>
+        <ScrollView contentContainerStyle={{flexGrow: 1}}>
+            <View style={styles.container}> 
+              <TouchableOpacity onPress={Keyboard.dismiss} activeOpacity={1}>
+                <View style = {styles.top}>
+                <Image style = {styles.logo} source={Logo} />
+                  <Text style= {styles.heading}> שאלון אימות</Text>
+                  
+                  {/* <Text>נתונים אלו יישלחו למנהל המערכת לצורך אימות נתוניך</Text> */}
+                </View>
 
-            <KeyboardAwareScrollView style = {styles.bottom}>
-              <View style = {styles.inputView}>
-                <Text style={styles.text}>האם שירתת ביחידה?</Text>
-                <View style ={styles.choiceView}>   
-                <TouchableOpacity 
-                onPress={()=>{setShow1(true);setShow2(false)}}
-                style={styles.choiceButton}>
-                  <Text style={styles.choiceText}>כן</Text>
-                </TouchableOpacity>
-                <TouchableOpacity 
-                onPress={()=>{setShow2(true); setShow1(false)}}
-                style={styles.choiceButton}>
-                  <Text style={styles.choiceText}>לא</Text>
-                </TouchableOpacity>  
-              </View>  
-            {show1?
-        
-            <View styel= {styles.show}>
-              <Text style={styles.text}>באיזה שנה?</Text>
-              <TextInput  
-                style={styles.showInput}
-                placeholderTextColor={"#fff"}
-                value={year}
-                onChangeText={text=>setYear(text)}
-              />
-              <Text style={styles.text}>באיזה מחזור?</Text>
-              <TextInput  
-                style={styles.showInput}
-                placeholderTextColor={"#fff"}
-                value={generation}
-                onChangeText={text=>setGeneration(text)}
-              />
-              <Text style={styles.text}>באיזה צוות?</Text>
-              <TextInput  
-                style={styles.showInput}
-                placeholderTextColor={"#fff"}
-                value={team}
-                onChangeText={text=>setTeam(text)}
-              />  
+              <KeyboardAwareScrollView style = {styles.bottom}>
+                <View style = {styles.inputView}>
+                  <Text style={styles.text}>האם שירתת ביחידה?</Text>
+                  <View style ={styles.choiceView}>   
+                  <TouchableOpacity 
+                  onPress={()=>{setShow1(true);setShow2(false)}}
+                  style={styles.choiceButton}>
+                    <Text style={styles.choiceText}>כן</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity 
+                  onPress={()=>{setShow2(true); setShow1(false)}}
+                  style={styles.choiceButton}>
+                    <Text style={styles.choiceText}>לא</Text>
+                  </TouchableOpacity>  
+                </View>  
+              {show1?
+          
+              <View styel= {styles.show}>
+                <Text style={styles.text}>באיזה שנה?</Text>
+                <TextInput  
+                  style={styles.showInput}
+                  placeholderTextColor={"#fff"}
+                  returnKeyType ='next'
+                  value={year}
+                  onChangeText={text=>setYear(text)}
+                  onSubmitEditing={() => { generationRef.current.focus(); }}
+                  blurOnSubmit={false}
+                />
+                <Text style={styles.text}>באיזה מחזור?</Text>
+                <TextInput  
+                  style={styles.showInput}
+                  placeholderTextColor={"#fff"}
+                  value={generation}
+                  onChangeText={text=>setGeneration(text)}
+                  ref = {input=>generationRef.current = input}
+                  onSubmitEditing={() => { teamRef.current.focus(); }}
+                  blurOnSubmit={false}
+                />
+                <Text style={styles.text}>באיזה צוות?</Text>
+                <TextInput  
+                  style={styles.showInput}
+                  placeholderTextColor={"#fff"}
+                  value={team}
+                  onChangeText={text=>setTeam(text)}
+                  ref = {input=>teamRef.current = input}
+                />  
+              </View>:null}
+            {show2?
+              <View>
+                <Text style={styles.text}>מדוע אתה מעוניין להצטרף?</Text>
+                <TextInput  
+                  style={styles.showInput}
+                  placeholderTextColor={"#fff"}
+                  value={why}
+                onChangeText={text=>setWhy(text)}
+                /> 
             </View>:null}
-          {show2?
-            <View>
-              <Text style={styles.text}>מדוע אתה מעוניין להצטרף?</Text>
-              <TextInput  
-                style={styles.showInput}
-                placeholderTextColor={"#fff"}
-                value={why}
-              onChangeText={text=>setWhy(text)}
-              /> 
-          </View>:null}
-          <TouchableOpacity style = {styles.buttons} onPress = {handleSignUp}>
-            <Text style = {styles.buttonText}>סיום</Text>
-          </TouchableOpacity>
-        </View>
-      </KeyboardAwareScrollView>
-      </TouchableOpacity>
-    </KeyboardAvoidingView>
-          );
+            <TouchableOpacity style = {styles.buttons} onPress = {handleSignUp}>
+              <Text style = {styles.buttonText}>סיום</Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAwareScrollView>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+    );
       };
       
   const keyExtractor = (item) => item.title;
@@ -325,10 +383,13 @@ const SignUp = props => {
           showNextButton={false}
           showDoneButton={false}
           showPrevButton={true}
+          dotClickEnabled={false}
+          activeDotStyle = {{}}
+          dotStyle = {{}}
           keyExtractor={keyExtractor}
           renderItem={renderItem}
           data={data}
-          
+          ref={ref=>{refference.current=ref}}
         />
       </View>
     );
@@ -425,7 +486,7 @@ const styles = StyleSheet.create({
     borderWidth:1.5,
     borderRadius:6,
     width:50,
-    height:25,
+    height:30,
     borderColor:'#fff',
     borderTopWidth:3,
     borderLeftWidth:3,
@@ -460,6 +521,7 @@ showInput:{
     padding:10,
     margin:10,
     textAlign:'right',
+    color:"white",
  },
       
    
