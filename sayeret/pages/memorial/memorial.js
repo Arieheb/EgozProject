b4c2ddb4c4cab9d06d8d15d636e62ac20670cccf
Idan Kideckel <idanbkideckel@gmail.com>
2022-06-17 20:01:57 +0300

    Update memorial.js

--- sayeret/pages/memorial/memorial.js
+++ sayeret/pages/memorial/memorial.js
@@ -1,9 +1,69 @@
-import {FlatList, View, StyleSheet, ImageBackground} from 'react-native';
+import {FlatList, View, StyleSheet, ImageBackground,SafeAreaView,Text, TextInput, Modal, TouchableOpacity} from 'react-native';
 import Blurp from './ModalTemp';
 import {auth, db} from '../../firebase';
 import { collection, onSnapshot, query ,orderBy } from 'firebase/firestore';
 import { useEffect,useState } from 'react';
 import memorial from "../../assets/Images/izkor.jpg";
+import Icons from 'react-native-vector-icons/Ionicons'
+
+
+const Search = (props) => {
+  const list = props.list;
+
+  const [searchList, setSearchList] = useState([]);
+  const [input, setInput] = useState("");
+  const [visible, setVisible] = useState(false);
+
+  //getting the list according to the input
+  const searcher = (name)=>{
+      setSearchList(list.filter(item=>(String(item.Name).includes(name))));
+  }
+
+  useEffect(()=>{setSearchList(list)},[])
+
+
+return (
+  <View>
+      <View style = {{maxWidth:'13%'}}>
+    <TouchableOpacity onPress={()=>setVisible(true)}>
+          <Icons name='search' color='white'size={55}/>
+    </TouchableOpacity>
+
+      </View>
+      <Modal visible={visible}>
+      <ImageBackground source={memorial} style = {{...styles.view, height:'100%'}}>
+          <SafeAreaView style={styles.top}>
+              <TouchableOpacity onPress={()=>{setVisible(false);setInput("");searcher("")}}>
+                  <Icons name='arrow-back' color='white' style={{transform:[{rotateY: '180deg'}]}} size={45}/>
+              </TouchableOpacity>
+          
+          {/*search bar*/}
+          <View style={styles.searchBar}>    
+              <TextInput 
+                  style={styles.textInput}
+                  placeholder='חפש'     
+                  value={input}
+                  onChangeText={text=>{setInput(text);searcher(text)}}
+                  placeholderTextColor="#7f8c8d"
+              />
+              <TouchableOpacity onPress={()=>{setInput("");searcher("")}}>
+                  <Text>X</Text>
+              </TouchableOpacity>
+          </View>
+          </SafeAreaView>
+          {/**the found list*/}
+              <FlatList data = {searchList}
+              keyExtractor = {item => item.Name}
+              renderItem = {(data) => <Blurp name = {data.item.Name} image={data.item.profilePic} info = {data.item.information} semitary = {data.item.semitary} part = {data.item.section} row = {data.item.row} graveNumber = {data.item.graveNumber} link = {data.item.link}  ></Blurp>}
+              numColumns = {3}
+              />
+            </ImageBackground>
+      </Modal>
+  </View>
+)
+}
+
+
 
 
 const Memorial = () => { 
@@ -32,19 +92,18 @@ const Memorial = () => {
 
     
      return (
-       <View style = {{width: '100%', height: "100%"}}>
-      <ImageBackground source={memorial} style = {styles.view}>
-       <View style = {styles.container}>
-        <FlatList data = {memoryInfo}
+      <View style = {{width: '100%', height: "100%"}}>
+        <ImageBackground source={memorial} style = {styles.view}>
+          <View style = {styles.container}>
+          <Search list={memoryInfo}/>
+            <FlatList data = {memoryInfo}
             keyExtractor = {item => item.Name}
             renderItem = {(data) => <Blurp name = {data.item.Name} image={data.item.profilePic} info = {data.item.information} semitary = {data.item.semitary} part = {data.item.section} row = {data.item.row} graveNumber = {data.item.graveNumber} link = {data.item.link}  ></Blurp>}
-            numColumns = {3}
->
-         </FlatList>
-         </View>
+            numColumns = {3}/>
+          </View>
 
-         </ImageBackground>
-         </View>
+        </ImageBackground>
+      </View>
     );
   };
   
@@ -68,6 +127,29 @@ const Memorial = () => {
         justifyContent: "center",
         
     },
+    textInput:{
+      width:"95%",
+      textAlign:"right",
+      height:'100%',
+      alignSelf:"flex-end",
+      borderRadius:5,
+      padding:5,
+      fontSize:18,
+      backgroundColor:"white"
+    },
+
+  searchBar:{
+      flexDirection:'row',
+      width:'85%',
+      height: 40,
+      borderColor:"gray",
+      borderWidth:1,
+      borderRadius:5,
+      justifyContent: 'flex-start',
+      alignItems:'center',
+      alignSelf:'center',
+      backgroundColor:"white"
+  },
 
 
   });
